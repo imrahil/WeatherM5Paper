90ae292c1e28f3c7f293e9ed07260e4e7126ccba
Jarek Szczepanski <imrahil@imrahil.com>
2021-11-14 10:53:02 +0100

    Displaying internal temperatures even when there is no internet connection

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,14 @@
 
 M5EPD_Canvas canvas(&M5.EPD);
 
-char temStr[10];
-char humStr[10];
-
-float temHere;
-float humHere;
-
 const char *ssid = WLAN_SSID;
 const char *password = WLAN_PASS;
-String town = "Ennetmoos";
-String country = "CH";
-const String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=" + town + "," + country + "&units=metric&APPID=";
+String api_town = "Ennetmoos";
+String api_country = "CH";
+const String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=" + api_town + "," + api_country + "&units=metric&APPID=";
 const String key = WEATHER_KEY;
 
-String payload = ""; // whole json
-String tmp = "";     // temperature
-String hum = "";     // humidity
-
-StaticJsonDocument<1000> doc;
+StaticJsonDocument<1024> doc;
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -37,6 +27,31 @@ NTPClient timeClient(ntpUDP);
 String formattedDate;
 String dayStamp;
 String timeStamp;
+
+void initWiFi() {
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to WiFi ..");
+  int count_try = 0;
+
+  while (WiFi.status() != WL_CONNECTED && count_try < WIFI_MAX_TRY)
+  {
+    Serial.print(".");
+    delay(1000);
+
+    count_try++;
+  }
+
+  Serial.println();
+
+  if (WiFi.status() == WL_NO_SSID_AVAIL) {
+    Serial.println("Impossible to establish WiFi connection!");
+    WiFi.disconnect();
+  } else {
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+}
 
 void setup(void)
 {
@@ -52,111 +67,114 @@ void setup(void)
   canvas.drawJpgFile(SD, "/back.jpg");
   canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
 
-  WiFi.begin(ssid, password);
+  initWiFi();
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(100);
+  if (WiFi.status() == WL_CONNECTED) {
+    timeClient.begin();
+    // Set offset time in seconds to adjust for your timezone, for example:
+    // GMT +1 = 3600
+    // GMT +8 = 28800
+    // GMT -1 = -3600
+    // GMT 0 = 0
+    timeClient.setTimeOffset(3600);
   }
-
-  timeClient.begin();
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-  timeClient.setTimeOffset(3600);
-  // getData();
 
   delay(100);
 }
 
-int i = 0;
-String tt = "";
-int count = 0;
-String curSeconds = "";
-
 void getData()
 {
+  String temperature = "--";
+  String humidity = "--";
+  String town = "--";
+  String pressure = "--";
+  String windSpeed = "--";
+
   // Check the current connection status
   if ((WiFi.status() == WL_CONNECTED))
   {
     HTTPClient http;
+    String payload = ""; // whole json
 
     http.begin(endpoint + key); // Specify the URL
     int httpCode = http.GET();  // Make the request
 
+    // check for the returning code
     if (httpCode > 0)
-    { // Check for the returning code
-      payload = http.getString();
-    }
-    else
     {
-      // Serial.println("Error on HTTP request");
+      payload = http.getString();
+
+      char input[1000];
+      payload.toCharArray(input, 1000);
+      deserializeJson(doc, input);
+
+      JsonObject main = doc["main"];
+
+      temperature = main["temp"].as<String>();
+      humidity = main["humidity"].as<String>();
+      pressure = main["pressure"].as<String>();
+
+      town = doc["name"].as<String>();
+      windSpeed = doc["wind"]["speed"].as<String>();
     }
 
     http.end(); // Free the resources
   }
 
-  char inp[1000];
-  payload.toCharArray(inp, 1000);
-  deserializeJson(doc, inp);
-
-  String tmp2 = doc["main"]["temp"];
-  String hum2 = doc["main"]["humidity"];
-  String town2 = doc["name"];
-  String pressure = doc["main"]["pressure"];
-  String visib = doc["main"]["visibility"];
-  String windSpeed = doc["wind"]["speed"];
-
-  tmp = tmp2;
-  hum = hum2;
-
   M5.SHT30.UpdateData();
-  temHere = M5.SHT30.GetTemperature();
-  humHere = M5.SHT30.GetRelHumidity();
+
+  float temHere = M5.SHT30.GetTemperature();
+  float humHere = M5.SHT30.GetRelHumidity();
 
   canvas.setFreeFont(&Orbitron_Bold_44);
-  canvas.drawString(tmp.substring(0, 4), 340, 260);
+  canvas.drawString(temperature.substring(0, 4), 340, 260);
   canvas.drawString(String(temHere).substring(0, 4), 180, 260);
 
   canvas.setFreeFont(&Orbitron_Bold_66);
-  canvas.drawString(hum, 348, 580);
+  canvas.drawString(humidity, 348, 580);
   canvas.drawString(String((int)humHere), 190, 580);
 
   canvas.setFreeFont(&Orbitron_Medium_25);
-  canvas.drawString(town2, 122, 784);
+  canvas.drawString(town, 122, 784);
   canvas.drawString(pressure + " hPa", 164, 846);
   canvas.drawString(windSpeed + " m/s", 122, 816);
 }
 
 void loop()
 {
-  getData();
-
-  while (!timeClient.update())
-  {
-    timeClient.forceUpdate();
+  if (WiFi.status() != WL_CONNECTED) {
+    initWiFi();
   }
 
-  // The formattedDate comes with the following format:
-  // 2018-05-28T16:00:13Z
-  // We need to extract date and time
-  formattedDate = timeClient.getFormattedDate();
-  Serial.println(formattedDate);
+  Serial.println("downloading data...");
+  getData();
 
-  int splitT = formattedDate.indexOf("T");
-  dayStamp = formattedDate.substring(0, splitT);
+  if (WiFi.status() == WL_CONNECTED) {
+    while (!timeClient.update())
+    {
+      timeClient.forceUpdate();
+    }
 
-  timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 1);
-  curSeconds = timeStamp.substring(6, 8);
-  String current = timeStamp.substring(0, 5);
+    // The formattedDate comes with the following format:
+    // 2018-05-28T16:00:13Z
+    // We need to extract date and time
+    formattedDate = timeClient.getFormattedDate();
+    Serial.println(formattedDate);
 
-  canvas.drawString(current, 140, 884);
+    int splitT = formattedDate.indexOf("T");
+    dayStamp = formattedDate.substring(0, splitT);
+
+    timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 1);
+    String current = timeStamp.substring(0, 5);
+
+    canvas.drawString(current, 140, 884);
+  }
+
   canvas.drawString(String(M5.getBatteryVoltage() / 1000.00), 430, 884);
 
   canvas.pushCanvas(0, 0, UPDATE_MODE_A2);
 
-  delay(700);
+  Serial.println("going into sleep mode...");
+  delay(2000);
   M5.shutdown(600);
 }
